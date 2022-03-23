d97051afc1fcab0e62ee5935bcd1d2cfe3ff555f
Jon-Erik Chandler <jchandler@icomputeconsulting.com>
2022-03-23 15:22:12 -0400

    rework code sample component to read from text file

--- js/components/CodeSample.js
+++ js/components/CodeSample.js
@@ -1,12 +1,34 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import "./CodeSample.styl"
 
-export default function CodeSample({ children, description, title }) {
+export default function CodeSample({
+	children,
+	description,
+	title,
+	codeTxtFile,
+}) {
+	const [codeAsTextStr, setCodeAsTextStr] = useState("")
+
+	// Convert text file to string of text
+	useEffect(() => {
+		if (codeTxtFile) {
+			fetch(codeTxtFile)
+				.then(r => r.text())
+				.then(text => {
+					setCodeAsTextStr(text)
+				})
+		}
+	}, [])
+
 	return (
 		<div className="CodeSample">
 			<h4>{title}</h4>
 			<p>{description}</p>
-			<div className="code">{children}</div>
+			<div className="code">
+				<pre>
+					<code>{codeAsTextStr}</code>
+				</pre>
+			</div>
 		</div>
 	)
 }
--- js/components/TaskMgmtApp.js
+++ js/components/TaskMgmtApp.js
@@ -4,8 +4,11 @@ import "./TaskMgmtApp.styl"
 import ScreenshotView from "./ScreenshotView"
 import CodeSample from "./CodeSample"
 
-import taskMgmtAgendaScreenshot from "url:../../statics/images/task-mgmt-screenshots/agenda.png"
-import taskMgmtScheduleScreenshot from "url:../../statics/images/task-mgmt-screenshots/schedule.png"
+import taskMgmtAgenda from "url:../../statics/images/task-mgmt-screenshots/agenda.png"
+import taskMgmtSchedule from "url:../../statics/images/task-mgmt-screenshots/schedule.png"
+
+import taskMgmtTimerFunction from "url:../../statics/docs/task-mgmt-timer-function.txt"
+import taskMgmtTimeCalculations from "url:../../statics/docs/task-mgmt-time-calculations.txt"
 
 export default function TaskMgmtApp() {
 	useEffect(() => {
@@ -35,195 +38,22 @@ export default function TaskMgmtApp() {
 			<h3>Screenshots</h3>
 
 			<div className="screenshots">
-				<ScreenshotView src={taskMgmtAgendaScreenshot} caption="Agenda view" />
-
-				<ScreenshotView
-					src={taskMgmtScheduleScreenshot}
-					caption="Scheduling view"
-				/>
+				<ScreenshotView src={taskMgmtAgenda} caption="Agenda view" />
+				<ScreenshotView src={taskMgmtSchedule} caption="Scheduling view" />
 			</div>
 
 			<h3>Code Snippets</h3>
 			<CodeSample
-				title="Timer Function"
+				title="Timer Function (JavaScript)"
 				description="A function to run every second and record progress for a task in progress."
-			>
-				<pre>
-					<code>{`timer = () => {
-		// Updates the current time in state and entry in progress if necessary
-		const currentTime = moment().toDate()
-		let newStateObj = { currentTime }
-
-		// Save state data to local storage every minute
-		let { secsToSave } = this.state
-		if (!secsToSave || secsToSave <= 0) {
-			this.saveStateToLocalStorage()
-			secsToSave = 60
-		}
-		newStateObj.secsToSave = secsToSave - 1
-
-		// Do some checks and updates if the timer is running
-		const { timeEntries, entryInProgress } = this.state
-		const timerRunning = Object.keys(entryInProgress).length > 0
-
-		if (timerRunning) {
-			//check to see if the entryInProgress has passed midnight by comparing currentTime with start of entryInProgress
-			if (
-				moment(currentTime).format("YYYY-MM-DD") !==
-				moment(entryInProgress.originalEntry.start).format("YYYY-MM-DD")
-			) {
-				let midnight = moment(moment(currentTime).format("YYYY-MM-DD")).toDate()
-				let { id, taskID, start } = entryInProgress.originalEntry
-
-				//create a new entry with the start as entryInProgress.originalEntry.start and end as midnight
-				this.endActiveEntry(id, taskID, start, midnight, false)
-
-				//add an new actual entry in progress starting at midnight
-				this.addTimeEntry(taskID, midnight, null, false)
-			} else {
-				//if the time in entry in progress has not passed midnight, just update the entry in progress
-				let updates = updateActiveEntryHelper(
-					entryInProgress.originalEntry.taskID,
-					entryInProgress.originalEntry.start,
-					this.state.tasks
-				)
-				updates.originalEntry = this.state.entryInProgress.originalEntry
-				newStateObj.entryInProgress = updates
-			}
-		}
-
-		this.setState(newStateObj)
-	}`}</code>
-				</pre>
-			</CodeSample>
+				codeTxtFile={taskMgmtTimerFunction}
+			/>
 
 			<CodeSample
-				title="Time Calculations"
+				title="Time Calculations (JavaScript and moment.js Javascript library)"
 				description="Functions to calculate the length of time between two timestamps and convert numbers of seconds into numbers of hours and minutes."
-			>
-				<pre>
-					<code>{`import moment from "moment"
-
-const timeCalculations = {
-	//takes a time duration in seconds and converts to hours, minutes, and number of hours in decimal value
-	formatTime: function (durationInSeconds) {
-		let hours, min, seconds, decimalHrs
-		const round = durationInSeconds >= 0 ? Math.floor : Math.ceil
-		hours = round(durationInSeconds / 3600)
-		min = round(durationInSeconds / 60 - hours * 60)
-		seconds = durationInSeconds - hours * 3600 - min * 60
-		decimalHrs = (durationInSeconds / 3600).toFixed(2)
-
-		return {
-			hours: Math.abs(hours),
-			min: Math.abs(min).toString().padStart(2, "0"),
-			seconds: Math.abs(seconds).toString().padStart(2, "0"),
-			decimalHrs: decimalHrs,
-		}
-	},
-
-	//Calculate the number of days from now to a certain date
-	formatDaysTill: function (taskDueDate) {
-		const daysTillDue = moment({ hours: 0 }).diff(moment(taskDueDate), "days")
-		let daysTillString
-		let formattedDate = moment(taskDueDate).format("MMM D")
-
-		if (daysTillDue === 0) {
-			daysTillString = formattedDate + " (today)"
-		} else if (daysTillDue === 1) {
-			daysTillString = formattedDate + " (yesterday)"
-		} else if (daysTillDue === -1) {
-			daysTillString = formattedDate + " (tomorrow)"
-		} else if (daysTillDue > 1) {
-			daysTillString = formattedDate + " (" + daysTillDue + " days ago)"
-		} else if (daysTillDue < -1) {
-			daysTillString =
-				formattedDate + " (in " + Math.abs(daysTillDue) + " days)"
-		} else if (taskDueDate === null) {
-			daysTillString = "(none)"
-		}
-
-		return {
-			string: daysTillString,
-			number: daysTillDue,
-		}
-	},
-
-	//creates a string with info about at task's priority
-	formatPriorityTdTitle: function (task) {
-		const { dueDate, prioritizedAt, priority } = task
-		let { deadline } = task
-
-		let tdTitle = ""
-		let priorityStr = ""
-
-		if (deadline === true) {
-			deadline = "deadline"
-		} else {
-			deadline = "target date"
-		}
-
-		if (priority === 0) {
-			priorityStr = "not prioritized"
-		} else if (priority === 1) {
-			priorityStr =
-				"marked high priority on " + moment(prioritizedAt).format("MMM D")
-		} else {
-			priorityStr = "prioritized on " + moment(prioritizedAt).format("MMM D")
-		}
-
-		if (dueDate === null) {
-			tdTitle = "No " + deadline + " specified; " + priority
-		} else {
-			tdTitle =
-				deadline.charAt(0).toUpperCase() +
-				deadline.slice(1) +
-				" on " +
-				moment(dueDate).format("MMM D") +
-				"; " +
-				priorityStr
-		}
-
-		return tdTitle
-	},
-
-	//calculate a time of day with number of seconds from midnight
-	findTimeFromSeconds: function (seconds) {
-		let hours = Math.floor(seconds / 3600)
-		let min = Math.floor((seconds % 3600) / 60)
-		let secs = Math.ceil(seconds - 3600 * hours - 60 * min)
-
-		let hrsPadded = hours.toString().padStart(2, "0")
-		let minPadded = min.toString().padStart(2, "0")
-		let secsPadded = secs.toString().padStart(2, "0")
-
-		let armyTime = hrsPadded + ":" + minPadded + ":" + secsPadded
-
-		let meridian = "AM"
-		if (hours === 12) {
-			meridian = "PM"
-		}
-
-		if (hours > 12 && hours < 24) {
-			hours -= 12
-			meridian = "PM"
-		}
-
-		if (hours === 24 || hours === 0) {
-			hours = 12
-		}
-
-		return {
-			meridianTime: hours + ":" + min + " " + meridian,
-			armyTime: armyTime,
-		}
-	},
-}
-
-module.exports = timeCalculations
-`}</code>
-				</pre>
-			</CodeSample>
+				codeTxtFile={taskMgmtTimeCalculations}
+			/>
 		</div>
 	)
 }
