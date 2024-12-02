d6fcf931fbd23c757def9f10562cd865ce182c20
Jon-Erik Chandler <jchandler@avantia-inc.com>
2024-12-02 14:42:31 -0500

    Fix scroll to top issue

--- src/components/PageContentWrapper.js
+++ src/components/PageContentWrapper.js
@@ -1,9 +1,13 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Loader from "./Loader"
 
 import "./PageContentWrapper.styl"
 
 export default function PageContentWrapper({ loading, children, centerChildren }) {
+    useEffect(() => {
+        document.querySelector(".page-content").scrollTo(0, 0)
+    }, [])
+
     return (
         <div className={`page-content-wrapper ${loading ? "loading" : ""}`}>
             {loading ? (
