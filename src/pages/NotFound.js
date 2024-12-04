bc0731264238c4577fe2745029b180ab098a35f6
Jon-Erik Chandler <jchandler@avantia-inc.com>
2024-12-04 08:48:12 -0500

    Formatting for page not found component

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import PageContentWrapper from '../components/PageContentWrapper'
+import ButtonLink from '../components/ButtonLink'
+
+import './NotFound.styl'
 
 function NotFound() {
-  return <div>NotFound</div>
+  return (
+    <PageContentWrapper>
+      <div className="not-found">
+        <p>The requested page could not be found.</p>
+        <ButtonLink route="/" text="Return to Homepage" />
+      </div>
+    </PageContentWrapper>
+  )
 }
 
 export default NotFound
