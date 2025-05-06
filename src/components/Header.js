910dd2d54110dda2d82a4a2e9443533cd7e7d98b
Jon-Erik Chandler <jchandler@avantia-inc.com>
2025-05-06 13:45:31 -0400

    Updates

--- src/components/Header.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-import './Header.styl'
-
-export default function Header({ html }) {
-  return <div className="header" dangerouslySetInnerHTML={{ __html: html }} />
-}
--- src/components/SubHeader.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import './SubHeader.styl'
-
-export default function SubHeader({ html }) {
-  return (
-    <div className="sub-header" dangerouslySetInnerHTML={{ __html: html }} />
-  )
-}
