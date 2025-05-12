6be73e81a42483a3ec72fe03133125db121323b9
Jon-Erik Chandler <jchandler@avantia-inc.com>
2025-05-12 15:35:41 -0400

    Add new env variable instead of hard-coding

--- src/services/prismic.js
+++ src/services/prismic.js
@@ -1,10 +1,8 @@
 import * as prismic from '@prismicio/client'
-const { REACT_APP_PRISMIC_ACCESS_TOKEN } = process.env
-
-const repositoryName = 'headless-cms-jonerik'
+const { REACT_APP_PRISMIC_ACCESS_TOKEN, REACT_APP_PRISMIC_REPO_NAME } = process.env
 const routes = []
 
-const client = prismic.createClient(repositoryName, {
+const client = prismic.createClient(REACT_APP_PRISMIC_REPO_NAME, {
   routes,
   REACT_APP_PRISMIC_ACCESS_TOKEN
 })
