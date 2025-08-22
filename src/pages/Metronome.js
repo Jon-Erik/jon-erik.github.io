62c0269a1216b1b74e33f6fbfe0872e3b89a9c8a
Jon-Erik Chandler <jonerik.chandler@gmail.com>
2025-08-22 09:28:32 -0400

    Basic beat visualization and highlighting

--- src/pages/Metronome.js
+++ src/pages/Metronome.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import PageContentWrapper from '../components/PageContentWrapper'
 import Button from '../components/Button'
 import woodblock from '../static/woodblock.mp3' // from pixabay
+import samples from '../static/samples'
 import './Metronome.styl'
 import useSound from 'use-sound'
 
@@ -10,213 +11,46 @@ const ONE_MINUTE = 60000
 function Metronome() {
   const [intervalState, setIntervalState] = useState(null)
   const [timeoutState, setTimeoutState] = useState([])
-  const [bpm, setBpm] = useState(72)
+  const [activeRhythm, setActiveRhythm] = useState([])
+  const [bpm, setBpm] = useState(60)
   const [play, opts] = useSound(woodblock)
 
-  // const beats = [
-  //   {
-  //     sound: 'woodblock',
-  //     play: true,
-  //   },
-  //   {
-  //     sound: null,
-  //     play: null,
-  //     subdivisions: [
-  //       {
-  //         sound: 'woodblock',
-  //         play: false,
-  //       },
-  //       {
-  //         sound: 'woodblock',
-  //         play: true,
-  //       }
-  //     ]
-  //   }
-  // ]
-
-  const beats = [
-    {
-      sound: 'woodblock',
-      play: false,
-      subdivisions: [
-        {
-          sound: 'woodblock',
-          play: true
-        },
-        {
-          sound: 'woodblock',
-          play: false,
-          subdivisions: [
-            {
-              sound: 'woodblock',
-              play: true
-            },
-            {
-              sound: 'woodblock',
-              play: true
-            },
-            {
-              sound: 'woodblock',
-              play: true
-            }
-          ]
-        }
-      ]
-    },
-    {
-      sound: 'woodblock',
-      play: false,
-      subdivisions: [
-        {
-          sound: 'woodblock',
-          play: true
-        },
-        {
-          sound: 'woodblock',
-          play: false,
-          subdivisions: [
-            {
-              sound: 'woodblock',
-              play: true
-            },
-            {
-              sound: 'woodblock',
-              play: true
-            },
-            {
-              sound: 'woodblock',
-              play: true
-            }
-          ]
-        }
-      ]
-    },
-    {
-      sound: 'woodblock',
-      play: true,
-      subdivisions: [
-        {
-          sound: 'woodblock',
-          play: true
-        },
-        {
-          sound: 'woodblock',
-          play: true
-        }
-      ]
-    },
-    {
-      sound: 'woodblock',
-      play: false,
-      subdivisions: [
-        {
-          sound: 'woodblock',
-          play: true
-        },
-        {
-          sound: 'woodblock',
-          play: false,
-          subdivisions: [
-            {
-              sound: 'woodblock',
-              play: true
-            },
-            {
-              sound: 'woodblock',
-              play: true
-            },
-            {
-              sound: 'woodblock',
-              play: true
-            }
-          ]
-        }
-      ]
-    },
-    {
-      sound: 'woodblock',
-      play: false,
-      subdivisions: [
-        {
-          sound: 'woodblock',
-          play: true
-        },
-        {
-          sound: 'woodblock',
-          play: false,
-          subdivisions: [
-            {
-              sound: 'woodblock',
-              play: true
-            },
-            {
-              sound: 'woodblock',
-              play: true
-            },
-            {
-              sound: 'woodblock',
-              play: true
-            }
-          ]
-        }
-      ]
-    },
-    {
-      sound: 'woodblock',
-      play: false,
-      subdivisions: [
-        {
-          sound: 'woodblock',
-          play: true
-        },
-        {
-          sound: 'woodblock',
-          play: true
-        },
-        {
-          sound: 'woodblock',
-          play: true
-        },
-        {
-          sound: 'woodblock',
-          play: true
-        },
-        {
-          sound: 'woodblock',
-          play: true
-        },
-        {
-          sound: 'woodblock',
-          play: true
-        }
-      ]
-    }
-  ]
+  /* Beats is an array of objects. Each object has either: 
+    - "sound" string (can be null if silent)
+    - "subdivisions" array with more objects in same format
+  */
+  const [beats, setBeats] = useState([{ sound: 'woodblock' }])
 
   const totalMsOfBeats = (ONE_MINUTE / bpm) * beats.length
 
   function getTimeouts(divisions) {
     const timeouts = []
 
-    function loopArr(arr, timeFromStartOfInterval, numOfDivisionsThisLevel) {
+    function loopArr(
+      arr,
+      timeFromStartOfInterval,
+      numOfDivisionsThisLevel,
+      parents = []
+    ) {
       arr.forEach((division, index) => {
         const timeToStartOfDivision =
           timeFromStartOfInterval +
           index * (totalMsOfBeats / numOfDivisionsThisLevel)
 
         if (division.subdivisions && division.subdivisions.length > 1) {
+          const subdivisionParents = parents.concat(index + 1)
           loopArr(
             division.subdivisions,
             timeToStartOfDivision,
-            numOfDivisionsThisLevel * division.subdivisions.length
+            numOfDivisionsThisLevel * division.subdivisions.length,
+            subdivisionParents
           )
         } else {
-          if (division.play && division.sound) {
-            timeouts.push({
-              ms: timeToStartOfDivision,
-              sound: division.sound
-            })
-          }
+          timeouts.push({
+            ms: timeToStartOfDivision,
+            sound: division.sound,
+            activeRhythm: parents.concat(index + 1)
+          })
         }
       })
     }
@@ -228,26 +62,35 @@ function Metronome() {
 
   function toggle() {
     if (intervalState) {
-      opts.stop()
       clearInterval(intervalState)
       setIntervalState(null)
-
       timeoutState.forEach((timeoutID) => {
         clearInterval(timeoutID)
       })
       setTimeoutState([])
+      setActiveRhythm([])
     } else {
       const timeouts = getTimeouts(beats)
       const timeoutIDs = []
 
       timeouts.forEach((timeout) => {
-        const timeoutID = setTimeout(() => play(), timeout.ms)
+        const timeoutID = setTimeout(() => {
+          if (timeout.sound) {
+            play()
+          }
+          setActiveRhythm(timeout.activeRhythm)
+        }, timeout.ms)
         timeoutIDs.push(timeoutID)
       })
 
       const interval = setInterval(() => {
         timeouts.forEach((timeout) => {
-          const timeoutID = setTimeout(() => play(), timeout.ms)
+          const timeoutID = setTimeout(() => {
+            if (timeout.sound) {
+              play()
+            }
+            setActiveRhythm(timeout.activeRhythm)
+          }, timeout.ms)
           if (!timeoutIDs.includes(timeoutID)) {
             timeoutIDs.push(timeoutID)
           }
@@ -259,12 +102,84 @@ function Metronome() {
     }
   }
 
+  function displayBeat(beat, index, level = 1, parents = []) {
+    const currentBeatLocation = parents.concat(index + 1)
+    let active = true
+
+    currentBeatLocation.forEach((beatLevel, blIndex) => {
+      if (activeRhythm[blIndex] && activeRhythm[blIndex] === beatLevel) {
+        // do nothing
+      } else {
+        active = false
+      }
+    })
+
+    return (
+      <div
+        className={`rhythm-division level-${level}`}
+        key={level + ':' + index}
+      >
+        <span className={active ? 'active' : ''}>{index + 1}</span>
+        <div className="rhythm-visualization">
+          {beat.subdivisions &&
+            beat.subdivisions.map((subdivision, subIndex) =>
+              displayBeat(
+                subdivision,
+                subIndex,
+                level + 1,
+                parents.concat(index + 1)
+              )
+            )}
+        </div>
+      </div>
+    )
+  }
+
   return (
     <PageContentWrapper>
       <div className="metronome">
-        <div>Metronome bpm = {bpm}</div>
+        <div>
+          <h3>Tempo</h3>
+          <p>
+            bpm ={' '}
+            <input
+              type="number"
+              value={bpm}
+              onChange={(e) => {
+                setBpm(Math.round(e.target.value))
+              }}
+              max={200}
+              min={0}
+              step={1}
+            />
+          </p>
+        </div>
+        <div>
+          <h3>Rhythm</h3>
+          <div className="rhythm-visualization">
+            {beats.map((beat, index) => displayBeat(beat, index))}
+          </div>
+        </div>
         <br />
         <Button text={intervalState ? 'Stop' : 'Play'} onClick={toggle} />
+        <div>
+          <h3>Samples</h3>
+          {samples.map((sample) => {
+            const { name, beats, bpm } = sample
+            return (
+              <div key={name}>
+                {name}{' '}
+                <Button
+                  text="set"
+                  onClick={() => {
+                    setBpm(bpm)
+                    setBeats(beats)
+                  }}
+                />
+              </div>
+            )
+          })}
+        </div>
       </div>
     </PageContentWrapper>
   )
---
+++ src/static/samples.js
@@ -0,0 +1,160 @@
+const samples = [
+  {
+    name: 'Mars (Gustav Holst)',
+    bpm: 140,
+    beats: [
+      {
+        subdivisions: [
+          {
+            sound: 'woodblock'
+          },
+          {
+            sound: 'woodblock'
+          },
+          {
+            sound: 'woodblock'
+          }
+        ]
+      },
+      {
+        sound: 'woodblock'
+      },
+      {
+        sound: 'woodblock'
+      },
+      {
+        subdivisions: [
+          {
+            sound: 'woodblock'
+          },
+          {
+            sound: 'woodblock'
+          }
+        ]
+      },
+      {
+        sound: 'woodblock'
+      }
+    ]
+  },
+  {
+    name: 'Bolero (Maurice Ravel)',
+    bpm: 72,
+    beats: [
+      {
+        subdivisions: [
+          {
+            sound: 'woodblock'
+          },
+          {
+            subdivisions: [
+              {
+                sound: 'woodblock'
+              },
+              {
+                sound: 'woodblock'
+              },
+              {
+                sound: 'woodblock'
+              }
+            ]
+          }
+        ]
+      },
+      {
+        subdivisions: [
+          {
+            sound: 'woodblock'
+          },
+          {
+            subdivisions: [
+              {
+                sound: 'woodblock'
+              },
+              {
+                sound: 'woodblock'
+              },
+              {
+                sound: 'woodblock'
+              }
+            ]
+          }
+        ]
+      },
+      {
+        subdivisions: [
+          {
+            sound: 'woodblock'
+          },
+          {
+            sound: 'woodblock'
+          }
+        ]
+      },
+      {
+        subdivisions: [
+          {
+            sound: 'woodblock'
+          },
+          {
+            subdivisions: [
+              {
+                sound: 'woodblock'
+              },
+              {
+                sound: 'woodblock'
+              },
+              {
+                sound: 'woodblock'
+              }
+            ]
+          }
+        ]
+      },
+      {
+        subdivisions: [
+          {
+            sound: 'woodblock'
+          },
+          {
+            subdivisions: [
+              {
+                sound: 'woodblock'
+              },
+              {
+                sound: 'woodblock'
+              },
+              {
+                sound: 'woodblock'
+              }
+            ]
+          }
+        ]
+      },
+      {
+        subdivisions: [
+          {
+            sound: 'woodblock'
+          },
+          {
+            sound: 'woodblock'
+          },
+          {
+            sound: 'woodblock'
+          },
+          {
+            sound: 'woodblock'
+          },
+          {
+            sound: 'woodblock'
+          },
+          {
+            sound: 'woodblock'
+          }
+        ]
+      }
+    ]
+  }
+]
+
+export default samples
