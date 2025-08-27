79349d2d0393bc0c84ee80498dea6ff8df032d5d
Jon-Erik Chandler <jonerik.chandler@gmail.com>
2025-08-27 09:40:22 -0400

    Remove piwik pro

--- index.js
+++ index.js
@@ -4,13 +4,8 @@ import { createRoot } from 'react-dom/client'
 import store from './src/state'
 import { Provider } from 'react-redux'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import PiwikPro from '@piwikpro/react-piwik-pro'
-
-const { REACT_APP_PIWIK_ID, REACT_APP_PIWIK_URL } = process.env
 
 const queryClient = new QueryClient()
-
-PiwikPro.initialize(REACT_APP_PIWIK_ID, REACT_APP_PIWIK_URL)
 
 const container = document.getElementById('root')
 const root = createRoot(container)
