3c97f01d3c8fd928befdc1238f049214f9a4d312
Jon-Erik Chandler <jonerik.chandler@gmail.com>
2025-08-27 09:40:41 -0400

    Basic working metronome with add/remove beats

--- src/pages/Metronome.js
+++ src/pages/Metronome.js
@@ -1,28 +1,111 @@
 import React, { useState } from 'react'
 import PageContentWrapper from '../components/PageContentWrapper'
 import Button from '../components/Button'
-import woodblock from '../static/woodblock.mp3' // from pixabay
-import samples from '../static/samples'
-import './Metronome.styl'
 import useSound from 'use-sound'
+import './Metronome.styl'
+
+import { MdOutlineDeleteForever } from 'react-icons/md'
+import { IoMdAddCircleOutline } from 'react-icons/io'
+
+// Sound files
+import woodblock from '../static/woodblock.mp3' // from pixabay
+import woodblockBright from '../static/wood-block-bright.wav'
+import woodblockChopped from '../static/wood-block-chopped.wav'
+
+// Examples
+import samples from '../static/samples'
+import { current } from '@reduxjs/toolkit'
 
 const ONE_MINUTE = 60000
 
 function Metronome() {
+  // IDs returned from setInterval()
   const [intervalState, setIntervalState] = useState(null)
+
+  // ID returned from setTimeout()
   const [timeoutState, setTimeoutState] = useState([])
+
+  // The rhythm division and subdivision(s) highlighted while playing
   const [activeRhythm, setActiveRhythm] = useState([])
-  const [bpm, setBpm] = useState(60)
-  const [play, opts] = useSound(woodblock)
 
-  /* Beats is an array of objects. Each object has either: 
-    - "sound" string (can be null if silent)
-    - "subdivisions" array with more objects in same format
+  // Metronome sounds init
+  const [playWoodblock, woodblockOpts] = useSound(woodblock)
+  const [playWoodblockBright, woodblockBrightOpts] = useSound(woodblockBright)
+  const [playWoodblockChopped, woodblockChoppedOpts] =
+    useSound(woodblockChopped)
+
+  function playSound(soundStr) {
+    switch (soundStr) {
+      case 'woodblock':
+        playWoodblock()
+        break
+      case 'woodblockBright':
+        playWoodblockBright()
+        break
+      case 'woodblockChopped':
+        playWoodblockChopped()
+        break
+      default:
+        // play nothing
+        break
+    }
+  }
+
+  /* Beat state - Beats is an array of objects. Each object has either: 
+  - "sound" string (can be null if silent)
+  - "subdivisions" array with more objects in same format
   */
-  const [beats, setBeats] = useState([{ sound: 'woodblock' }])
-
+  const defaultBeat = { sound: 'woodblock' }
+  const [bpm, setBpm] = useState(60)
+  const [beats, setBeats] = useState([Object.assign({}, defaultBeat)])
   const totalMsOfBeats = (ONE_MINUTE / bpm) * beats.length
 
+  function addBeatOrSubdivision(parents = []) {
+    let newBeats = [...beats]
+    let currentArr = newBeats
+
+    // Using the parent indices, find the array or nested array to add to
+    parents.forEach((parentIndex) => {
+      // Create subdivisions array if it does not exist
+      if (currentArr[parentIndex] && !currentArr[parentIndex].subdivisions) {
+        currentArr[parentIndex].subdivisions = []
+      }
+
+      currentArr = currentArr[parentIndex].subdivisions
+    })
+
+    // If array is empty add two default beats. Otherwise just add one
+    if (!currentArr.length) {
+      currentArr.push(
+        Object.assign({}, defaultBeat),
+        Object.assign({}, defaultBeat)
+      )
+    } else {
+      currentArr.push(Object.assign({}, defaultBeat))
+    }
+
+    setBeats(newBeats)
+  }
+
+  function removeBeatOrSubdivision(index, parents = []) {
+    const newBeats = [...beats]
+    let currentArr = newBeats
+
+    // Using the parent indices, find the array or nested array to add to
+    parents.forEach((parentIndex) => {
+      currentArr = currentArr[parentIndex].subdivisions
+    })
+
+    if (!parents.length && currentArr.length <= 1) {
+      // Do nothing - don't remove the only beat at the top level
+    } else {
+      currentArr.splice(index, 1)
+    }
+
+    setBeats(newBeats)
+  }
+
+  // Determine the points in time to play metronome sounds in the time length of beat cycle
   function getTimeouts(divisions) {
     const timeouts = []
 
@@ -38,7 +121,7 @@ function Metronome() {
           index * (totalMsOfBeats / numOfDivisionsThisLevel)
 
         if (division.subdivisions && division.subdivisions.length > 1) {
-          const subdivisionParents = parents.concat(index + 1)
+          const subdivisionParents = parents.concat(index)
           loopArr(
             division.subdivisions,
             timeToStartOfDivision,
@@ -60,6 +143,7 @@ function Metronome() {
     return timeouts
   }
 
+  // Stop/start metronome
   function toggle() {
     if (intervalState) {
       clearInterval(intervalState)
@@ -75,9 +159,7 @@ function Metronome() {
 
       timeouts.forEach((timeout) => {
         const timeoutID = setTimeout(() => {
-          if (timeout.sound) {
-            play()
-          }
+          playSound(timeout.sound)
           setActiveRhythm(timeout.activeRhythm)
         }, timeout.ms)
         timeoutIDs.push(timeoutID)
@@ -86,9 +168,7 @@ function Metronome() {
       const interval = setInterval(() => {
         timeouts.forEach((timeout) => {
           const timeoutID = setTimeout(() => {
-            if (timeout.sound) {
-              play()
-            }
+            playSound(timeout.sound)
             setActiveRhythm(timeout.activeRhythm)
           }, timeout.ms)
           if (!timeoutIDs.includes(timeoutID)) {
@@ -102,6 +182,7 @@ function Metronome() {
     }
   }
 
+  // Recursive function to display beat and subdivisions and determine if beat/subdivision is active
   function displayBeat(beat, index, level = 1, parents = []) {
     const currentBeatLocation = parents.concat(index + 1)
     let active = true
@@ -120,6 +201,18 @@ function Metronome() {
         key={level + ':' + index}
       >
         <span className={active ? 'active' : ''}>{index + 1}</span>
+        <span
+          className="remove-beat"
+          onClick={() => addBeatOrSubdivision([...parents, index])}
+        >
+          <IoMdAddCircleOutline />
+        </span>
+        <span
+          className="remove-beat"
+          onClick={() => removeBeatOrSubdivision(index, parents)}
+        >
+          <MdOutlineDeleteForever />
+        </span>
         <div className="rhythm-visualization">
           {beat.subdivisions &&
             beat.subdivisions.map((subdivision, subIndex) =>
@@ -127,7 +220,7 @@ function Metronome() {
                 subdivision,
                 subIndex,
                 level + 1,
-                parents.concat(index + 1)
+                parents.concat(index)
               )
             )}
         </div>
@@ -156,6 +249,9 @@ function Metronome() {
         </div>
         <div>
           <h3>Rhythm</h3>
+          <Button text="Add Beat" onClick={() => addBeatOrSubdivision()} />
+          <br />
+          <br />
           <div className="rhythm-visualization">
             {beats.map((beat, index) => displayBeat(beat, index))}
           </div>
